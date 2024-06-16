667b2cee51ef6d12a68e65440457b179205351a4
Dominik Zogg <dominik.zogg@hotmail.com>
2024-06-16 20:13:16 +0200

    autoprefixer, postcss, tailwindcss to dev dependencies, upgrades

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
     include: ['tests/**/*.test.*'],
     coverage: {
       all: true,
